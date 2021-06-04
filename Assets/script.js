93d44b9781e0085b3541a85977592e7ed7b82093
Dauphine Burns <dauphineburns@gmail.com>
2021-06-04 16:46:25 -0400

    updates 06/04

---
+++ Assets/script.js
@@ -0,0 +1,43 @@
+
+// Event listener for going to maps page
+
+// Location search
+
+// Event listener for search for location
+
+// Fetch data from apis to show on map
+fetch('link to api', {
+    method: 'GET',
+})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        // add in what we really want it to do with the data
+        console.log(data)
+});
+
+// Fetch song data
+fetch('link to api', {
+    method: 'GET',
+})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        // add in what we really want it to do with the data
+        console.log(data)
+});
+
+
+// Link hiking info to song data
+
+// Modal for hike information when clicked on
+
+// Event listener for start hike button that prompts music to play
+
+// Local storage favorite hikes
+
+// Event listener for favorite hikes button
+
+// Page for favorite hikes
