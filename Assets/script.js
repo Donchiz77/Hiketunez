6e866f0408a6625f9cbbca7088d435b21af8154e
Dauphine Burns <dauphineburns@gmail.com>
2021-06-05 10:06:09 -0400

    updates starting 06/05

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,35 @@
 
 var searchBtn = document.querySelector('#searchBtn');
 var userInput = document.querySelector('#userInput');
+var playMusicBtn = document.querySelector('.playMusicBtn');
+var favoriteBtn = document.querySelector('#favoritesBtn');
 
 // Location search
 
 // Event listener for search for location
 searchBtn.addEventListener('click', function () {
-    hikeInfo(userInput.value.trim())
-    console.log(value);
-});
+    hikeInfo(userInput.value.trim());
+    console.log(value); 
 
-// Fetch data from apis to show on map
-function (hikeInfo) {
-    fetch('link to api', {
-        method: 'GET',
-    })
-        .then(function (response) {
-            return response.json();
+
+    // Fetch data from apis to show on map
+    function (hikeInfo) {
+        fetch('link map api', {
+            method: 'GET',
         })
-        .then(function (data) {
-            // add in what we really want it to do with the data
-            console.log(data)
-    });
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                // add in what we really want it to do with the data
+                console.log(data)
+        });
 
 };
 
 // Fetch song data
-fetch('link to api', {
+playMusicBtn.addEventListener('click', function () {
+    fetch('https://openwhyd.org/adrien/playlist/61', {
     method: 'GET',
 })
     .then(function (response) {
