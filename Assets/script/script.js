46c8ef07b3e7c71e39915742abced7d5dbb42dfd
Dauphine Burns <dauphineburns@gmail.com>
2021-06-06 17:36:07 -0400

    map api

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -7,49 +7,44 @@ var saveHikeBtn = document.querySelector('saveHikeBtn');
 // Modal Variables
 var modalCard = document.querySelector('.modal-card');
 var modalTitle = document.querySelector('.card-title');
-var cardInfo = document.querySelector('card-info');
+var cardInfo = document.querySelector('.card-info');
 
 
 // Location search
 
 // Event listener for search for location
+// This function works. Do not edit!
 // searchBtn.addEventListener('click', function () {
+//     hikeInfo(userInput.value)
+//     console.log(userInput.value); 
+// });
+
+
+// Need to fix the jumping to bing when searched.
+var map;
+window.onload = function hikeInfo() {
+    console.log('hello');
+    fetch('https://www.bing.com/api/maps/mapcontrol?setmkt=en-us&key=gtuAaeBHapf7XBFBQ4ZV~Xdl98hjuASFWeHOqINZKow~AkyglvfQ4jn1wMwELZatWxyprn5sIvHDEq7GdkuwXyitgVXbPQQW_T7cAmGmJlZZ')
+    map = new Microsoft.Maps.Map(document.getElementById('map'), {showSearchBar: true});
     
-//     hikeInfo(userInput.value.trim());
-//     console.log(value); 
-
-
-//     // Fetch data from apis to show on map
-//     developer.nps.gov/api/v1/places
-//     function test (hikeInfo) {
-//         fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=M0Bd4k4wGIILywauG2LxueJtOIgJjtc8nBrEF7Jd', {
-//         })
-//             .then(function (response) {
-//                 return response.json();
-//             })
-//             .then(function (data) {
-//                 // add in what we really want it to do with the data
-//                 console.log(data)
-//         });
-
-//     };
-// });    
+}
+    
 
   
 
 // Fetch song data
-playMusicBtn.addEventListener('click', function () {
-    fetch('https://openwhyd.org/yvad/playlist/5', {
-    method: 'GET',
-})
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        // add in what we really want it to do with the data
-        console.log(data)
-});
-});
+// playMusicBtn.addEventListener('click', function () {
+//     fetch('https://openwhyd.org/yvad/playlist/5', {
+//     method: 'GET',
+// })
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         // add in what we really want it to do with the data
+//         console.log(data)
+// });
+// });
 
 
 // Link hiking info to song data
